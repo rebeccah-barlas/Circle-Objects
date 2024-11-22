6a1102fb27785ad48414c329392d64c2c71c635d
rebeccah-barlas <rebeccah.m.barlas@gmail.com>
2024-11-22 15:54:10 -0500

    first commit

---
+++ Circle Objects/Circle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_Objects
+{
+    internal class Circle
+    {
+        private double Radius { get; set; }
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public double CalculateDiameter()
+        {
+            return Radius *2;
+        }
+        public double CalculateCircumfrence()
+        {
+            return 2 * Math.PI * Radius;
+        }
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+        public void Grow()
+        {
+            Radius = Radius * 2;
+        }
+        public double GetRadius()
+        {
+            return Radius;
+        }
+    }
+}
---
+++ Circle Objects/Program.cs
@@ -0,0 +1,46 @@
+﻿using Circle_Objects;
+
+Console.WriteLine("Welcome to the Circle Tester!");
+Console.WriteLine("Please enter a radius - you may enter a decimal if desired");
+double userRadius = 0;
+bool validNumber = false;
+while (validNumber == false)
+{
+    try
+    {
+        userRadius = double.Parse(Console.ReadLine());
+        validNumber = true;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Please enter a valid number");
+    }
+}
+
+Circle circle = new Circle(userRadius);
+DisplayCircleProperties(circle);
+
+bool agreeToGrow = true;
+while (agreeToGrow == true)
+{
+    Console.WriteLine("Should the circle grow? (y/n)");
+    string userGrow = Console.ReadLine();
+    if (userGrow == "y")
+    {
+        Console.WriteLine("The circle is magically growing!");
+        circle.Grow();
+        DisplayCircleProperties(circle);
+    }
+    else if (userGrow == "n")
+    {
+        agreeToGrow = false;
+        double finalRadius = circle.GetRadius();
+        Console.WriteLine($"The final radius is: {finalRadius}. Goodbye!");
+    }
+}
+static void DisplayCircleProperties(Circle circle)
+{
+    Console.WriteLine($"The diameter is: {circle.CalculateDiameter()}");
+    Console.WriteLine($"The circumference is: {circle.CalculateCircumfrence()}");
+    Console.WriteLine($"The area is: {circle.CalculateArea()}");
+}
